1767124b0fc23f5d505726d585951096a6b1c491
Nico993 <nicolascab993@gmail.com>
2022-10-07 19:56:36 -0300

    fecha minima de inicio arreglado

--- client/src/components/Promociones/FormInputs/FechaInicioYFin.js
+++ client/src/components/Promociones/FormInputs/FechaInicioYFin.js
@@ -4,12 +4,35 @@ import Form from 'react-bootstrap/Form';
 
 function FechaInicioYFin(props){
     const [date, setDate] = useState(new Date());
-    const [dateString, setDateString] = useState()
+    const [dateString, setDateString] = useState("");
 
     useEffect(()=>{
         setDate(new Date());
-        setDateString(date.getFullYear() + "-0" + (date.getMonth() + 1) + "-0" + date.getDate() + "T" + date.getHours() + ":" + date.getMinutes()); 
+        setDateString(getDateString());
     },[]);
+
+    function getDateString(){
+        let year = date.getFullYear();
+        let month = date.getMonth() + 1;
+        let day = date.getDate();
+        let hour = date.getHours();
+        let minutes = date.getMinutes();
+        if(month.toString().length === 1){
+            month = "0" + month;
+        }
+        if(day.toString().length === 1){
+            day = "0" + day;
+        }
+        if(hour.toString().length === 1){
+            hour = "0" + hour;
+        }
+        if(minutes.toString().length === 1){
+            minutes = "0" + minutes;
+        }
+        console.log(year + "-" + month + "-" + day + "T" + hour + ":" + minutes);
+        return (year + "-" + month + "-" + day + "T" + hour + ":" + minutes);
+    }
+
     return  <Form.Group className="mb-3 row">
     <div className="col col-lg-6">
     <Form.Label className="label">Fecha Inicio</Form.Label>
